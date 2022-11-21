bbe4d6de131d9c9042bc0d348f9ede2188e3825d
itay <itayblecher1998@gmail.com>
2022-11-21 11:02:55 +0200

    automation works

--- init.sh
+++ init.sh
@@ -21,8 +21,6 @@ pod_num=0
 while read ip; do
   echo "Running scanner for ip:$ip"
   kubectl run tsunami-$pod_num --restart='OnFailure' --image dizex98/tsunami:2 -- --ip-v4-target=$ip
-  #--scan-results-local-output-format=JSON \
-  #--scan-results-local-output-filename=logs/tsunami-output.json
   pod_num=`expr $pod_num + 1`
 done < list.ip
 
@@ -41,10 +39,14 @@ while [ $flag -eq 0 ]; do
   done < list.status
 done
 
-# removing tsunami pods and showing logs.
+# removing tsunami pods, showing and saving logs.
+mkdir -p logs
 for (( i=0; i<$pod_num; i++ ))
 do
+  # Saving logs
+  kubectl logs tsunami-$i > logs/tsunami-$i.logs
   echo "Last logs from tsunami-$i:"
+  # Show number of vulnerabilities and amount of scan's time
   kubectl logs tsunami-$i | tail -n 7
   kubectl delete pod tsunami-$i
 done
